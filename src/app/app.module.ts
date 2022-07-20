669d828ee1c1eb2f149150c40084816221e84bc6
XoR <gerardo.solis@nearshoretechnology.com>
2022-07-20 17:16:55 -0500

    Angular home added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularHomeComponent } from './angular-home/angular-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularHomeComponent
   ],
   imports: [
     BrowserModule,
