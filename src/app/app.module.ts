b0de0b156663d93e11b33b2fd803f2ee1663a99d
XoR <gerardo.solis@nearshoretechnology.com>
2022-07-26 14:02:56 -0500

    Added Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularHomeComponent } from './angular-home/angular-home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { CSSContainerComponent } from './css-container/css-container.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularHomeComponent
+    AngularHomeComponent,
+    NavBarComponent,
+    CSSContainerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
