fc9ab71d7012d3bf2f3f859a5fc787781f45149f
XoR <gerardo.solis@nearshoretechnology.com>
2022-08-04 17:01:04 -0500

    Button hover glowing effect

--- src/app/css-container/css-container.module.ts
+++ src/app/css-container/css-container.module.ts
@@ -1,4 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { ButtonHoverGlowingComponent } from "./button-hover-glowing/button-hover-glowing.component";
 import { CSSContainerRoutingModule } from "./css-container-routing.module";
 import { CSSContainerComponent } from "./css-container.component";
 import { NavMenuIndicatorComponent } from "./nav-menu-indicator/nav-menu-indicator.component";
@@ -9,6 +10,7 @@ import { TextAnimationWaterComponent } from './text-animation-water/text-animati
 @NgModule({
     imports: [
         CSSContainerRoutingModule,
+        ButtonHoverGlowingComponent
     ],
     declarations: [
         CSSContainerComponent,
